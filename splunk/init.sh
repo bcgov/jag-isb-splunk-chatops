b0c769c4009ffe0990c7f008b46fc9bf10dedd1a
Alan Dodge <alan.dodge@gov.bc.ca>
2020-09-28 11:38:38 -0700

    Updated init for new docker, added missing env vars to compose, activated mvn profiles in dockerfile

--- splunk/init.sh
+++ splunk/init.sh
@@ -4,15 +4,15 @@
 while [ "${HEALTH_CHECK}" != "{\"text\":\"HEC is healthy\",\"code\":17}" ]
 do
   echo "Waiting for splunk ..."
-  HEALTH_CHECK=$(curl -ksS https://splunk:8088/services/collector/health)
+  HEALTH_CHECK=$(curl -ksS https://notification-service-splunk:8088/services/collector/health)
   sleep 3
 done
 
 # Create HEC token
 sleep 5
 echo "Creating HEC token ..."
-curl -X POST -ksS --user admin:"${SPLUNK_PASSWORD}" https://splunk:8089/services/data/inputs/http -d name=rsbc_hec_token -d token="${SPLUNK_TOKEN}"
+curl -X POST -ksS --user admin:"${SPLUNK_PASSWORD}" https://notification-service-splunk:8089/services/data/inputs/http -d name=notification_service_hec_token -d token="${SPLUNK_TOKEN}"
 
 # Test
 echo "Testing log write ..."
-curl -ksS "https://splunk:8088/services/collector" -H "Authorization: Splunk ${SPLUNK_TOKEN}"  -d '{"event": "Hello, world!", "sourcetype": "manual"}'
+curl -ksS "https://notification-service-splunk:8088/services/collector" -H "Authorization: Splunk ${SPLUNK_TOKEN}"  -d '{"event": "Hello, world!", "sourcetype": "manual"}'
